34228cd3f8edd50f233370efc6b9c7af0a1b6fb8
Fransiskus Andika Setiawan <dikakoko@icloud.com>
2022-01-02 17:09:26 +0700

    merge to main

---
+++ app/build/generated/source/buildConfig/debug/ids/dika/recipes/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ids.dika.recipes;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ids.dika.recipes";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String BASE_API = "https://www.themealdb.com/api/json/v1/1/";
+}
